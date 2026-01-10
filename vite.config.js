ff842bb7bd15bb254419eacfc288a21c4e43aa76
Glauco Pater <glauco.pater@gmail.com>
2026-01-10 18:45:53 +0100

    Fix for running on localhost

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // This is required for Vite to work correctly with CodeSandbox
-const server = process.env.APP_ENV === "sandbox" ? { hmr: { clientPort: 443 } } : {};
+const server0 = process.env.APP_ENV === "sandbox" ? { hmr: { clientPort: 443 } } : {};
+
+const server = {
+  host: "127.0.0.1", // Add this line for main HTTP server
+  hmr: {
+    // Remove or comment host line here too
+    strictPort: true,
+  },
+  port: Number(process.env.VITE_PORT) || 5173,
+  open: "/", // Ensures clean open
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
